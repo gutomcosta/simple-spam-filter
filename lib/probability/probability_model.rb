3127ea5457929b674fd3abda620a370f2f3d1266
Luiz costa <gutomcosta@gmail.com>
2012-02-06 23:40:59 -0200

    melhorando a api e incluindo conceito de probablidade complementar

--- lib/probability/probability_model.rb
+++ lib/probability/probability_model.rb
@@ -7,8 +7,23 @@ module Probability
 		end
 
 		def prior(event)
-			return 0 unless (self.possible_worlds.include?(event))
-			(1.0 / @possible_worlds.count.to_f)
+			return 0 unless is_there_event?(event)
+			(number_of_occurrences_for(event) / @possible_worlds.count.to_f)
+		end
+
+		def complementary(event)
+			raise "Event #{event} doesn't exists" if not is_there_event?(event)
+			1 - prior(event)
+		end
+
+		private	
+
+		def number_of_occurrences_for(event)
+			@possible_worlds.count {|e| e == event}
+		end
+
+		def is_there_event?(event)
+			self.possible_worlds.include?(event)
 		end
 
 	end
--- spec/probability/probability_model_spec.rb
+++ spec/probability/probability_model_spec.rb
@@ -1,20 +1,25 @@
 require_relative '../../lib/probability/probability_model'
 
-def possible_words_for_a_dice
+def sample_space_for_a_dice
 	# it's possible values when we rolling a dice
 	["1","2","3","4","5","6"]
+end
+
+def sample_space_for_weather
+	["sunny", "sunny", "rain", "cloudy", "cloudy","snow","rain", "sunny"]
+	
 end
 
 module Probability
 	describe "Probability Model" do 
 
 		it "should allow to define possible worlds " do
-			probability_model = ProbabilityModel.new possible_words_for_a_dice
+			probability_model = ProbabilityModel.new sample_space_for_a_dice
 			probability_model.possible_worlds.should_not be_nil
 		end
 
-		context "about the 'Uncondicional or Prior' probability a dice with [1,2,3,4,5,6] possibilities" do 
-			let(:probability_model) { ProbabilityModel.new possible_words_for_a_dice }
+		context "about the 'Uncondicional or Prior' probability for a dice with [1,2,3,4,5,6] possibilities" do 
+			let(:probability_model) { ProbabilityModel.new sample_space_for_a_dice }
 		
 			it "should 0,166 to the probability P(1)" do
 				probability_model.prior("1").should be_within(0.02).of(0.166)
@@ -23,6 +28,33 @@ module Probability
 			it "should return 0 to the probabilty p(7)" do 
 				probability_model.prior("7").should be_within(0.020).of(0)
 			end
+		end
+
+		context 'about the "Uncondicional or Prior" probabilty for a weather possibilities ["sunny", "sunny", "rain", "cloudy", "cloudy","snow","rain", "sunny"]' do
+			let(:probability_model) { ProbabilityModel.new sample_space_for_weather }
+			
+			it "should be 0.37 to the probabilty P(sunny)" do
+				probability_model.prior("sunny").should be_within(0.02).of(0.37)
+			end
+
+			it "should be 0.12 to the probabilty P(snow)" do 
+				probability_model.prior("snow").should be_within(0.02).of(0.12)
+			end
+		end
+
+		context 'about the complementary events, it is P(not Event) = 1 - P(event)' do 
+			let(:probability_model) { ProbabilityModel.new sample_space_for_weather }
+
+			it "should be 0.62 to the probability P(not sunny)" do 
+				probability_model.complementary("sunny").should be_within(0.02).of(0.62)
+			end
+
+			it "should raise an error for a complementary probability that doesn't exists" do 
+				expect {
+					probability_model.complementary("invalid event")
+				}.to raise_error
+			end
+
 		end
 	end
 end
